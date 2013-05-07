5b7b84eb0481bef5bcf9a049ec8bb5ff7f2eec47
Stephen Hohner <hohner13@hotmail.com>
2013-05-07 14:33:18 -0400

    Simple menu system.

--- data/examples/blockcity/init.js
+++ data/examples/blockcity/init.js
@@ -1,17 +1,71 @@
+var titleMenuButtonForgroundColor = Rgba(255, 255, 255, 200);
+var titleMenuButtonForgroundColorHovered = Rgba(255, 255, 255, 255);
+var titleMenuButtonForgroundColorActive = Rgba(80, 0, 0, 255);
+var titleMenuFont = Font("monospace", 26);
+
 engine.setTicksPerSecond(30);
 var surface = new GraphicsSurface("BlockCity", Size(800, 600));
 var titleContainer = new GraphicsContainer(surface);
 surface.background = Rgb(50, 0, 0);
 
+var curScreen = null;
+
 function ready() {
-    this.play = new GraphicsText("Play", Font("Ubuntu", 30), titleContainer);
-    play.foreground = Rgba(255, 255, 255, 100);
+    this.playGameButton = new GraphicsText("Play!", titleMenuFont, titleContainer);
+    playGameButton.foreground = titleMenuButtonForgroundColor;
+    playGameButton.mouseEntered.connect(function() {
+        playGameButton.animate("foreground", titleMenuButtonForgroundColorHovered);
+    });
+
+    playGameButton.mouseLeft.connect(function() {
+        playGameButton.animate("foreground", titleMenuButtonForgroundColor);
+    });
+
+    playGameButton.mousePressed.connect(function(/*point, buttonType*/) {
+        playGameButton.killAnimation("foreground");
+        playGameButton.foreground = titleMenuButtonForgroundColorActive;
+        engine.debug("TBD: Init and swap to a playContainer");
+    });
+
+
+    this.levelEditorButton = new GraphicsText("Level Editor", titleMenuFont, titleContainer);
+    levelEditorButton.foreground = titleMenuButtonForgroundColor;
+    levelEditorButton.mouseEntered.connect(function() {
+        levelEditorButton.animate("foreground", titleMenuButtonForgroundColorHovered);
+    });
+
+    levelEditorButton.mouseLeft.connect(function() {
+        levelEditorButton.animate("foreground", titleMenuButtonForgroundColor);
+    });
+
+
+    this.exitGameButton = new GraphicsText("Exit", titleMenuFont, titleContainer);
+    exitGameButton.foreground = titleMenuButtonForgroundColor;
+    exitGameButton.mouseEntered.connect(function() {
+        exitGameButton.animate("foreground", titleMenuButtonForgroundColorHovered);
+    });
+
+    exitGameButton.mouseLeft.connect(function() {
+        exitGameButton.animate("foreground", titleMenuButtonForgroundColor);
+    });
+
+
+    this.resizeTitleScreen = function(size) {
+        playGameButton.setPos(size.width / 2 - playGameButton.width / 2, (size.height / 2 - playGameButton.height / 2) - 64);
+        levelEditorButton.setPos(size.width / 2 - levelEditorButton.width / 2, (size.height / 2 - levelEditorButton.height / 2));
+        exitGameButton.setPos(size.width / 2 - exitGameButton.width / 2, (size.height / 2 - exitGameButton.height / 2) + 64);
+    }
+    curScreen = titleContainer;
 }
 
 surface.connected.connect(ready);
 surface.resized.connect(function(size){
     engine.debug(size);
     titleContainer.setSize(surface.size());
+    if(curScreen == titleContainer) {
+        resizeTitleScreen(size);
+    }
 });
 titleContainer.setSize(surface.size());
 //engine.tick.connect(func);
+
